b6f2cfd0c121480ffca6946a2063dbb004fc374b
ShuMei <wolforced@gmail.com>
2013-06-03 18:34:01 +0800

    version 1.0

---
+++ src/me/shumei/open/oks/baidutieba/MainActivity.java
@@ -0,0 +1,15 @@
+package me.shumei.open.oks.baidutieba;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+
+}
